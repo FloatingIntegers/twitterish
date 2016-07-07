337a9dc0da9f082fc9b5aaa3bc7b00c46c355d4a
Emma Deacon <emma_deacon10@hotmail.com>
2016-07-07 15:41:33 +0100

    Creates handler.js file

--- src/handler.js
+++ src/handler.js
@@ -1,3 +1,21 @@
-function handler(req, res){
-  
+const fs = require('fs');
+const url = require('url');
+
+function handler(req, res) {
+  const urlData = url.parse(req.url);
+  const pathName = urlData.pathname;
+
+  if (pathName === '/') {
+    fs.readFile(`${__dirname}/../public/index.html`, (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        res.end('<h2>404 File not found</h2>');
+      } else {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.end(data);
+      }
+    });
+  }
 }
+
+module.exports = handler;
