29bb817dd6d294e09a61d2e39ca130b080749422
Sofia <emmapohjalainen@gmail.com>
2016-07-08 17:53:21 +0100

    add websockets

--- src/server.js
+++ src/server.js
@@ -1,7 +1,36 @@
 const http = require('http');
 const handler = require('./handler.js');
 const port = process.env.PORT || 4000;
+const socketIO = require('socket.io');
 
-http.createServer(handler).listen(port);
+const app = http.createServer(handler);
+const io = socketIO(app);
 
-console.log(`Server is listening on port + ${port}`);
+const redis = require('redis');
+
+app.listen(port);
+
+
+io.on('connection', socket => {
+  console.log('user connected');
+
+  const client = redis.createClient();
+  client.lrange('tweets', 0, -1, (err, reply) => {
+    const data = reply.map(JSON.parse);
+    socket.emit('tweet-update', data);
+  });
+
+  socket.on('tweet', (data) => {
+    console.log(data);
+    const dateId = Date.now().toString();
+    const tweetObj = Object.assign({ dateId }, data);
+    client.lpush(['tweets', JSON.stringify(tweetObj)], () => {
+      client.lrange('tweets', 0, -1, (err, reply) => {
+        const data = reply.map(JSON.parse);
+        io.emit('tweet-update', data);
+      });
+    });
+  });
+});
+
+console.log(`Server is listening on port ${port}`);
