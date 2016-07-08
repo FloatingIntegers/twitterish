dd63406d472d4b2d7cca4b27931365be19eb05bd
Matt Glover <matthewglover@me.com>
2016-07-08 14:22:05 +0100

    clean up handler

--- src/handler.js
+++ src/handler.js
@@ -1,39 +1,12 @@
-const fs = require('fs');
 const url = require('url');
-const {
-  getPostData,
-  getContentType,
-} = require('./request-helpers');
+const { handleHomeRoute, handlePublicRoutes, handleSetTweet } = require('./route-handlers');
 
 function handler(req, res) {
   const pathName = url.parse(req.url).pathname;
 
-  if (pathName === '/') {
-    fs.readFile(`${__dirname}/../public/index.html`, (err, data) => {
-      if (err) {
-        res.writeHead(404, { 'Content-type': 'text/html' });
-        res.end('<h2>404 File not found</h2>');
-      } else {
-        res.writeHead(200, { 'Content-type': 'text/html' });
-        res.end(data);
-      }
-    });
-  } else if (pathName === '/set-tweet') {
-    getPostData(req, (data) => {
-      // eslint-disable-next-line no-console
-      console.log(JSON.parse(data));
-    });
-  } else {
-    fs.readFile(`${__dirname}/../public/${pathName}`, (err, data) => {
-      if (err) {
-        res.writeHead(404, { 'Content-type': 'text/html' });
-        res.end('<h2>404 File not found</h2>');
-      } else {
-        res.writeHead(200, { 'Content-type': getContentType(pathName) });
-        res.end(data);
-      }
-    });
-  }
+  if (pathName === '/') handleHomeRoute(res);
+  else if (pathName === '/set-tweet') handleSetTweet(req, res);
+  else handlePublicRoutes(pathName, res);
 }
 
 module.exports = handler;
