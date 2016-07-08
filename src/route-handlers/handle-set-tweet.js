b3c79014a36db7764813cbe8dfccd0cbcbfd4794
Matt Glover <matthewglover@me.com>
2016-07-08 15:57:19 +0100

    fix redis errors for travis

--- src/route-handlers/handle-set-tweet.js
+++ src/route-handlers/handle-set-tweet.js
@@ -1,10 +1,10 @@
 const { getPostData } = require('../request-helpers');
 const redis = require('redis');
-const client = redis.createClient();
 
 
 const handleSetTweet = (request, response) => {
   getPostData(request, (data) => {
+    const client = redis.createClient();
     const dateId = Date.now().toString();
     const tweetObj = Object.assign({ dateId }, JSON.parse(data));
     client.lpush(['tweets', JSON.stringify(tweetObj)]);
