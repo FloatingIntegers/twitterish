721c66159c5a0a41c9d3c2d765ebb72e297087c0
Sofia <emmapohjalainen@gmail.com>
2016-07-08 16:30:50 +0100

    gets tweets from redis
    and passes them to client side as objets

--- src/handler.js
+++ src/handler.js
@@ -1,11 +1,15 @@
 const url = require('url');
-const { handleHomeRoute, handlePublicRoutes, handleSetTweet } = require('./route-handlers');
+const { handleHomeRoute,
+        handlePublicRoutes,
+        handleSetTweet,
+        handleGetTweets } = require('./route-handlers');
 
 function handler(req, res) {
   const pathName = url.parse(req.url).pathname;
 
   if (pathName === '/' && req.method === 'GET') handleHomeRoute(res);
   else if (pathName === '/set-tweet' && req.method === 'POST') handleSetTweet(req, res);
+  else if (pathName === '/get-tweets' && req.method === 'GET') handleGetTweets(req, res);
   else handlePublicRoutes(pathName, res);
 }
 
