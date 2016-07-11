3259ea06c28917a5998f7d68920442e5c63f3286
Sofia <emmapohjalainen@gmail.com>
2016-07-11 17:01:42 +0100

    responds to issues #38 and #32

--- public/js/main.js
+++ public/js/main.js
@@ -10,8 +10,13 @@
 
   // Emit tweet event with current tweet input
   document.getElementById('tweetButton').addEventListener('click', () => {
+    if (tweetInput.value.length < 1){
+      return;
+    }
     socket.emit('tweet', { tweet: tweetInput.value });
+    tweetInput.value = '';
   });
+
 
   // Update tweet display on tweet-update event
   socket.on('tweet-update', updateTweets);
